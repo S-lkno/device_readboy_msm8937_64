c2a8e27d16c3b2173b11cd2029c002c1305e73e8
S-lkno <97375687+S-lkno@users.noreply.github.com>
2026-07-29 19:34:37 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_msm8937_64-user
-add_lunch_combo omni_msm8937_64-userdebug
-add_lunch_combo omni_msm8937_64-eng
+#add_lunch_combo omni_msm8937_64-user
+#add_lunch_combo omni_msm8937_64-userdebug
+#add_lunch_combo omni_msm8937_64-eng

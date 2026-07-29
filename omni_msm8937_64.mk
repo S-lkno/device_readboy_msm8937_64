d2fcde89efa7d20f227ce656bc7525d53cc23df4
S-lkno <97375687+S-lkno@users.noreply.github.com>
2026-07-29 19:32:15 +0800

    Update omni_msm8937_64.mk

--- omni_msm8937_64.mk
+++ omni_msm8937_64.mk
@@ -3,14 +3,7 @@
 # Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
-#
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit from msm8937_64 device
 $(call inherit-product, device/readboy/msm8937_64/device.mk)
